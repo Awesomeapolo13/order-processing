fde71937b799b8a628cba3fb7a6f9270076ba0d0
Aleksey Logachev <a.logachev@crpo.su>
2025-01-07 14:12:09 +0300

    DEV Finished get basket use case

--- src/Application/UseCase/GetFullBasketUseCase.php
+++ src/Application/UseCase/GetFullBasketUseCase.php
@@ -8,57 +8,108 @@ use App\Application\Assembler\ResponseAssemblerInterface;
 use App\Application\Request\GetFullBasketRequest;
 use App\Application\Response\GetFullBasketResponse;
 use App\Domain\Entity\Basket;
+use App\Domain\Exception\BasketConcurrentModificationException;
+use App\Domain\Factory\BasketFactory;
 use App\Domain\Repository\BasketRepositoryInterface;
-use App\Domain\ValueObject\BasketType;
-use App\Domain\ValueObject\Cost;
 use App\Domain\ValueObject\Region;
-use App\Domain\ValueObject\Weight;
-use DateTime;
-use DateTimeImmutable;
-use Doctrine\ORM\NonUniqueResultException;
-use Doctrine\ORM\NoResultException;
+use Doctrine\DBAL\LockMode;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\OptimisticLockException;
+use Psr\Log\LoggerInterface;
+use Throwable;
 
 final class GetFullBasketUseCase
 {
     public function __construct(
         private readonly BasketRepositoryInterface $basketRepository,
         private readonly ResponseAssemblerInterface $responseAssembler,
+        private readonly BasketFactory $basketFactory,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly LoggerInterface $logger,
     ) {
     }
 
+    /**
+     * @throws Throwable
+     */
     public function __invoke(GetFullBasketRequest $dto): GetFullBasketResponse
     {
+        $userId = $dto->userId;
         $region = new Region($dto->regionCode);
+
         try {
-            $basket = $this->basketRepository->findBasketByUserId($dto->userId);
-        } catch (NoResultException $exception) {
-            // ToDo: Logg the error
-            $basket = new Basket(
-                new Region($dto->regionCode),
-                new BasketType(false, true, false),
-                new DateTime(),
-                new DateTimeImmutable(),
-                new DateTimeImmutable(),
-                new Cost('0.00'),
-                new Cost('0.00'),
-                new Cost('0.00'),
-                new Weight('0.000'),
-                0,
-                $dto->userId,
-                null,
-            );
-        } catch (NonUniqueResultException $exception) {
-            // ToDo: Logg the error
-            // ToDo: Handle a double of the basket
+            return $this->processBasketRequest($userId, $region);
+        } catch (OptimisticLockException $exception) {
+            $this->logger->error('Concurrent modification detected', [
+                'user_id' => $userId,
+                'exception' => $exception->getMessage()
+            ]);
+
+            throw new BasketConcurrentModificationException($userId, previous: $exception);
+        } catch (Throwable $exception) {
+            $this->logger->error('Unexpected error during basket processing', [
+                'user_id' => $dto->userId,
+                'exception' => $exception->getMessage()
+            ]);
+
+            throw $exception;
         }
+    }
+
+    private function processBasketRequest(int $userId, Region $region): GetFullBasketResponse
+    {
+        try {
+            $basket = $this->getOrCreateBasket($userId, $region);
+
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+
+            return $this->responseAssembler->createResponse($basket);
+        } catch (Throwable $exception) {
+            $this->entityManager->rollback();
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * @throws OptimisticLockException
+     */
+    private function getOrCreateBasket(int $userId, Region $region): Basket
+    {
+        $this->entityManager->beginTransaction();
+
+        $basket = $this->basketRepository->findActiveBasketByUserId($userId);
+
+        if ($basket === null) {
+            $this->logger->info('No basket found, creating new', [
+                'user_id' => $userId,
+                'region' => $region->getRegionCode(),
+            ]);
+
+            $basket = $this->basketFactory->createDefaultAuthorized($userId, $region);
+            $this->entityManager->persist($basket);
+        }
+        $this->entityManager->lock($basket, LockMode::OPTIMISTIC);
 
         if (!$basket->getRegion()->isSame($region)) {
-            // ToDo: Delete a Basket and create a new one
-
-            // ToDo: Задать вопрос клауду, как лучше сделать,
-            //  удалить корзину сразу или же помечать корзины их на удаление и сносить кроной.
+            $this->logger->info('Region mismatch, recreating basket', [
+                'user_id' => $userId,
+                'old_region' => $basket->getRegion()->getRegionCode(),
+                'new_region' => $region->getRegionCode(),
+            ]);
+            $basket = $this->recreateBasketWithNewRegion($userId, $region);
         }
 
-        return $this->responseAssembler->createResponse($basket);
+        return $basket;
+    }
+
+    private function recreateBasketWithNewRegion(int $userId, Region $region): Basket
+    {
+        $this->basketRepository->setBasketDeleted($userId);
+        $newBasket = $this->basketFactory->createDefaultAuthorized($userId, $region);
+        $this->entityManager->persist($newBasket);
+
+        return $newBasket;
     }
 }
