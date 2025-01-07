d09bb0e91598dfb9211daf399b19a3ce782d84c0
Aleksey Logachev <a.logachev@crpo.su>
2025-01-07 14:12:18 +0300

    DEV Fix controller

--- src/Infrastructure/Http/Controller/GetFullBasketController.php
+++ src/Infrastructure/Http/Controller/GetFullBasketController.php
@@ -8,22 +8,24 @@ use App\Application\Request\GetFullBasketRequest;
 use App\Application\UseCase\GetFullBasketUseCase;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\HttpKernel\Attribute\MapQueryString;
-use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
+use Throwable;
 
 #[Route(path: '/api/v1/basket', name: 'app.basket.full')]
-class GetFullBasketController extends AbstractController
+final class GetFullBasketController extends AbstractController
 {
     public function __construct(
         private readonly GetFullBasketUseCase $getFullBasketUseCase,
     ) {
     }
 
+    /**
+     * @throws Throwable
+     */
     public function __invoke(
         #[MapQueryString] GetFullBasketRequest $request,
     ): Response {
-        return $this->json(($this->getFullBasketUseCase)($request));
+        return $this->json(['basket' => ($this->getFullBasketUseCase)($request)]);
     }
 }
