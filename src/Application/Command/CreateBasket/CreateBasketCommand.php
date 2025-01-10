b5dc9f8febe8563557dc5e2fafa077c7c59a22dc
Aleksey Logachev <a.logachev@crpo.su>
2025-01-11 00:00:51 +0300

    DEV Added CreateBasketCommand and handler

---
+++ src/Application/Command/CreateBasket/CreateBasketCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Command\CreateBasket;
+
+use App\Application\Command\CommandInterface;
+use App\Domain\ValueObject\Region;
+
+readonly class CreateBasketCommand  implements CommandInterface
+{
+    public function __construct(
+        public int $userId,
+        public Region $region
+    ) {
+    }
+}
---
+++ src/Application/Command/CreateBasket/CreateBasketHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Command\CreateBasket;
+
+use App\Application\Command\CommandHandlerInterface;
+use App\Domain\Factory\BasketFactory;
+use App\Domain\Repository\BasketRepositoryInterface;
+
+class CreateBasketHandler implements CommandHandlerInterface
+{
+    public function __construct(
+        private readonly BasketFactory $basketFactory,
+        private readonly BasketRepositoryInterface $basketRepository,
+    ) {
+    }
+
+    /**
+     * @return int - basket ID
+     */
+    public function __invoke(CreateBasketCommand $command): int
+    {
+        $basket = $this->basketFactory->createDefaultAuthorized($command->userId, $command->region);
+        $this->basketRepository->save($basket);
+
+        return $basket->getId();
+    }
+}
