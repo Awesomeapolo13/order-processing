3de13aba52f58a11c3c9f937ac367d4f22441b14
Aleksey Logachev <a.logachev@crpo.su>
2025-01-11 00:00:34 +0300

    DEV Added FindFullBasketQuery and handler

---
+++ src/Application/Query/FindFullBasket/FindFullBasketQuery.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Query\FindFullBasket;
+
+use App\Application\Query\QueryInterface;
+
+readonly class FindFullBasketQuery implements QueryInterface
+{
+    public function __construct(
+        public int $userId
+    ) {
+    }
+}
