c4f92a74196b8a63007836fae4f0f0229bc97929
Aleksey Logachev <a.logachev@crpo.su>
2025-01-12 15:01:45 +0300

    DEV Added application entity manager interface and implemented it

---
+++ src/Infrastructure/Database/EntityManager/DoctrineEntityManager.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Database\EntityManager;
+
+use App\Application\Database\EntityManager\TransactionalEntityManagerInterface;
+use DateTimeInterface;
+use Doctrine\ORM\EntityManagerInterface as DoctrineEntityManagerInterface;
+use Doctrine\ORM\OptimisticLockException;
+
+class DoctrineEntityManager implements TransactionalEntityManagerInterface
+{
+    public function __construct(
+        private readonly DoctrineEntityManagerInterface $entityManager,
+    ) {
+    }
+
+    public function beginTransaction(): void
+    {
+        $this->entityManager->beginTransaction();
+    }
+
+    /**
+     * @throws OptimisticLockException
+     */
+    public function lock(object $entity, int $lockMode, DateTimeInterface|int|null $lockVersion): void
+    {
+        $this->entityManager->lock($entity, $lockMode, $lockVersion);
+    }
+
+    public function persist(object $entity): void
+    {
+        $this->entityManager->persist($entity);
+    }
+
+    public function flush(): void
+    {
+        $this->entityManager->flush();
+    }
+
+    public function commit(): void
+    {
+        $this->entityManager->commit();
+    }
+
+    public function rollback(): void
+    {
+        $this->entityManager->rollback();
+    }
+}
