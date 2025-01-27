4dd70acc4ec096da3b99f06de6c58ba271c99335
Aleksey Logachev <a.logachev@crpo.su>
2025-01-27 10:00:08 +0300

    Feature-add-product-to-basket Add product from catalog command and handler have been added

---
+++ src/Application/Command/AddProductFromCatalog/AddProductFromCatalogCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Command\AddProductFromCatalog;
+
+use App\Application\Command\CommandInterface;
+
+final readonly class AddProductFromCatalogCommand implements CommandInterface
+{
+    public function __construct(
+        public int $basketId,
+        public string $supCode,
+        public ?int $quantity,
+        public ?string $weight,
+        public bool $isPack = false,
+    ) {
+    }
+}
---
+++ src/Application/Command/AddProductFromCatalog/AddProductFromCatalogHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Command\AddProductFromCatalog;
+
+use App\Application\Command\CommandHandlerInterface;
+
+class AddProductFromCatalogHandler implements CommandHandlerInterface
+{
+    public function __invoke(AddProductFromCatalogCommand $command): void
+    {
+        // TODO: Implement add product from catalog
+    }
+}
