ac92b32ecd4f13515b369a708191528568324fa0
Aleksey Logachev <a.logachev@crpo.su>
2025-02-26 12:59:00 +0300

    Feature-add-product-to-basket Finished with short basket usecase class

--- src/Application/UseCase/AddNewProductFromCatalogUseCase.php
+++ src/Application/UseCase/AddNewProductFromCatalogUseCase.php
@@ -4,21 +4,40 @@ declare(strict_types=1);
 
 namespace App\Application\UseCase;
 
+use App\Application\Command\AddProductFromCatalog\AddProductFromCatalogCommand;
+use App\Application\Command\CommandBusInterface;
+use App\Application\Query\FindShortBasket\FindShortBasketQuery;
+use App\Application\Query\QueryBusInterface;
 use App\Application\Request\AddNewProductFromCatalogRequest;
 use App\Application\Response\ShortBasketResponse;
-use DateTime;
+use App\Domain\ValueObject\Region;
 
 final class AddNewProductFromCatalogUseCase
 {
+    public function __construct(
+        private readonly CommandBusInterface $commandBus,
+        private readonly QueryBusInterface $queryBus,
+    ) {
+    }
+
     public function __invoke(AddNewProductFromCatalogRequest $request): ShortBasketResponse
     {
-        return new ShortBasketResponse(
-            id: 1,
-            isExpress: true,
-            isDelivery: false,
-            orderDate: new DateTime(),
-            totalDiscountCost: '0.00',
-            basketItems: []
+        $this->commandBus->execute(
+            new AddProductFromCatalogCommand(
+                $request->userId,
+                new Region($request->regionCode),
+                $request->supCode,
+                (int)$request->quantity,
+                $request->weight,
+                $request->isPack,
+            )
+        );
+
+        return $this->queryBus->execute(
+            new FindShortBasketQuery(
+                $request->userId,
+                $request->regionCode,
+            )
         );
     }
 }
