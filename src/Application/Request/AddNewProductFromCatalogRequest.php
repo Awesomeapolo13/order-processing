f54fdb8ca3f3d9d56e31cfbef786320dace7d12b
Aleksey Logachev <a.logachev@crpo.su>
2025-02-26 15:06:23 +0300

    Feature-add-product-to-basket Modified add product from catalog request

--- src/Application/Request/AddNewProductFromCatalogRequest.php
+++ src/Application/Request/AddNewProductFromCatalogRequest.php
@@ -7,8 +7,11 @@ namespace App\Application\Request;
 readonly class AddNewProductFromCatalogRequest
 {
     public function __construct(
+        public int $userId,
+        public int $regionCode,
         public string $supCode,
         public string $quantity,
+        public string $weight,
         public bool $isPack,
     ) {
     }
