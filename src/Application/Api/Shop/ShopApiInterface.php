c660685f862c665fdd76591bbe3e75cd6f76ae48
Aleksey Logachev <a.logachev@crpo.su>
2025-04-19 14:19:49 +0300

    Feature-basket-setup Added API interfaces.

---
+++ src/Application/Api/Shop/ShopApiInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Api\Shop;
+
+use App\Domain\ValueObject\ShopInterface;
+
+interface ShopApiInterface
+{
+    public function findShop(int $shopNumber): ?ShopInterface;
+}
