8ce7ad362af5d5229d497464432a8f68669369c9
Aleksey Logachev <a.logachev@crpo.su>
2025-04-19 13:41:20 +0300

    Feature-basket-setup Added SetUpBasket call.

--- src/Application/UseCase/SetUpBasketUseCase.php
+++ src/Application/UseCase/SetUpBasketUseCase.php
@@ -4,12 +4,33 @@ declare(strict_types=1);
 
 namespace App\Application\UseCase;
 
+use App\Application\Command\CommandBusInterface;
+use App\Application\Command\SetUpBasket\SetUpBasketCommand;
 use App\Application\Request\SetUpBasketRequest;
 
 final class SetUpBasketUseCase
 {
+    public function __construct(
+        private readonly CommandBusInterface $commandBus,
+    ) {
+    }
+
+    /**
+     * @throws \DateMalformedStringException
+     */
     public function __invoke(SetUpBasketRequest $request): void
     {
-        // TODO: Implement __invoke() method.
+        $this->commandBus->execute(
+            new SetUpBasketCommand(
+                $request->userId,
+                $request->regionCode,
+                new \DateTimeImmutable($request->orderDate),
+                $request->isDelivery,
+                $request->shopNumber,
+                $request->slotNumber,
+                $request->distance,
+                $request->longDuration,
+            )
+        );
     }
 }
